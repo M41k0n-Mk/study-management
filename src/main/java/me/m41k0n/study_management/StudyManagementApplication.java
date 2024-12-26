38856b1e149739e3a98f06d43ffa654ffa3cb59f
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2024-12-26 15:37:13 -0300

    init: first commit

---
+++ src/main/java/me/m41k0n/study_management/StudyManagementApplication.java
@@ -0,0 +1,13 @@
+package me.m41k0n.study_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyManagementApplication.class, args);
+	}
+
+}
