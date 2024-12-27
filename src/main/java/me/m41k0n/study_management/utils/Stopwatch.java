61c07f56eba2ca749e482a23f8479c935857dd1c
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2024-12-26 23:15:47 -0300

    fix: Implement a mechanism to create a new instance of time when one is already running.

--- src/main/java/me/m41k0n/study_management/utils/Stopwatch.java
+++ src/main/java/me/m41k0n/study_management/utils/Stopwatch.java
@@ -7,26 +7,35 @@ import java.util.TimerTask;
 
 public class Stopwatch {
     private LocalTime time;
-    private final Timer timer;
+    private Timer timer;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private boolean isRunning;
 
     public Stopwatch() {
         time = LocalTime.of(0, 0, 0);
         timer = new Timer();
+        isRunning = false;
     }
 
     public void start() {
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                time = time.plusSeconds(1);
-                System.out.println(time.format(formatter));
-            }
-        }, 0, 1000);
+        if (!isRunning) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    time = time.plusSeconds(1);
+                    System.out.println(time.format(formatter));
+                }
+            }, 0, 1000);
+            isRunning = true;
+        }
     }
 
     public void stop() {
-        timer.cancel();
+        if (isRunning) {
+            timer.cancel();
+            isRunning = false;
+        }
     }
 
     public void reset() {
